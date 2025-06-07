8522aff516571981469d4ae348b82a8353237440
YamahaPuff <bomborassclaat689@gmail.com>
2025-06-07 12:39:31 +0100

    Add files via upload

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -1,79 +1,122 @@
-package backend;
+import flixel.addons.display.FlxBackdrop;
 
-import flixel.util.FlxGradient;
-import flixel.FlxSubState;
-
-class CustomFadeTransition extends FlxSubState {
-	public static var finishCallback:Void->Void;
-	var isTransIn:Bool = false;
-	var transBlack:FlxSprite;
-	var transGradient:FlxSprite;
-
-	var duration:Float;
-	public function new(duration:Float, isTransIn:Bool)
+var tranDur:Float = .35;
+public var altTrans:Bool;
+function create() {
+	if(!nextFrameSkip)
 	{
-		this.duration = duration;
-		this.isTransIn = isTransIn;
-		super();
-	}
 
-	override function create()
+		transitionTween.cancel();
+		var out = newState != null;
+		
+		remove(blackSpr);
+		remove(transitionSprite);
+
+
+		altTrans = FlxG.save.data.moyTransitionType;
+
+		if (altTrans){
+			add(up = new FlxBackdrop(Paths.image("menus/extras/Spikey"),FlxAxes.X, 0, 0));
+			add(down = new FlxBackdrop(Paths.image("menus/extras/Spikey"),FlxAxes.X, 0, 0));
+
+			up.flipY = true;
+			up.velocity.x = -200;
+			up.y = -600;
+
+			down.velocity.x = 200;
+			down.y = 1400;
+
+
+			if(out){
+				// for (thin in [up,down]) thin.alpha = 0;
+
+				FlxTween.tween(up,{y: -280},tranDur,{ease: FlxEase.circInOut});
+				FlxTween.tween(down,{y: 1060},tranDur,{ease: FlxEase.circInOut,onComplete:
+					function(t:FlxTween) {
+						done();
+					}
+				});
+			} else {
+				// for (thin in [up,down]) thin.alpha = 1;
+				up.y = -280;
+				down.y = 1060;
+
+				FlxTween.tween(up,{y: -680},tranDur,{ease: FlxEase.circInOut});
+
+				FlxTween.tween(down,{y: 1400},tranDur,{ease: FlxEase.circInOut, onComplete:
+					function(t:FlxTween) {
+						done();
+					}
+				});
+			}
+
+		} else {
+			add(left = new FlxSprite(-779).loadGraphic(Paths.image("trans/left")));
+			add(right = new FlxSprite(1280).loadGraphic(Paths.image("trans/right")));
+	
+			add(middle = new FlxSprite().loadGraphic(Paths.image("trans/middle")));
+			middle.screenCenter();
+	
+			if(out){
+				left.x = -779;
+				right.x = 1280;
+				middle.alpha = 0;
+				middle.y -= 100;
+	
+				FlxTween.tween(middle,{alpha: 1,y: middle.y + 100},tranDur);
+				FlxTween.tween(left,{x: 0},tranDur);
+	
+				FlxTween.tween(right,{x: 500},tranDur,{onComplete:
+					function(t:FlxTween) {
+						done();
+					}
+				});
+	
+			} else {
+				left.x = 0;
+				right.x = 500;
+				middle.alpha = 1;
+				middle.screenCenter();
+	
+				for (each in [left,right,middle]){
+					each.flipY = true;
+				}
+	
+				FlxTween.tween(middle,{alpha: 0,y: middle.y + 100},tranDur);
+	
+				FlxTween.tween(left,{x: -779},tranDur,{ease: FlxEase.circIn});
+				
+				FlxTween.tween(right,{x: 1280},tranDur,{ease: FlxEase.circIn,onComplete:
+					function(t:FlxTween) {
+						done();
+					}
+				});
+			}
+			transitionCamera.scroll.y = 0;
+		}
+
+		FlxG.sound.play(Paths.sound("menu/Transition" + (out ? 'Out' : 'In')));
+	}
+	else
 	{
-		cameras = [FlxG.cameras.list[FlxG.cameras.list.length-1]];
-		var width:Int = Std.int(FlxG.width / Math.max(camera.zoom, 0.001));
-		var height:Int = Std.int(FlxG.height / Math.max(camera.zoom, 0.001));
-		transGradient = FlxGradient.createGradientFlxSprite(1, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		transGradient.scale.x = width;
-		transGradient.updateHitbox();
-		transGradient.scrollFactor.set();
-		transGradient.screenCenter(X);
-		add(transGradient);
-
-		transBlack = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
-		transBlack.scale.set(width, height + 400);
-		transBlack.updateHitbox();
-		transBlack.scrollFactor.set();
-		transBlack.screenCenter(X);
-		add(transBlack);
-
-		if(isTransIn)
-			transGradient.y = transBlack.y - transBlack.height;
-		else
-			transGradient.y = -transGradient.height;
-
-		super.create();
+		done();
 	}
+}
 
-	override function update(elapsed:Float) {
-		super.update(elapsed);
+function done()
+{
+	if (newState != null)
+		FlxG.switchState(newState);
 
-		final height:Float = FlxG.height * Math.max(camera.zoom, 0.001);
-		final targetPos:Float = transGradient.height + 50 * Math.max(camera.zoom, 0.001);
-		if(duration > 0)
-			transGradient.y += (height + targetPos) * elapsed / duration;
+	new FlxTimer().start(1.2, ()-> {
+		if (altTrans)
+			for (item in [up,down]){
+				FlxTween.tween(item,{alpha: 0},1.2,{onComplete: function (twn:FlxTween) {
+					close();
+				}});
+			}
 		else
-			transGradient.y = (targetPos) * elapsed;
-
-		if(isTransIn)
-			transBlack.y = transGradient.y + transGradient.height;
-		else
-			transBlack.y = transGradient.y - transBlack.height;
-
-		if(transGradient.y >= targetPos)
-		{
 			close();
-		}
-	}
-
-	// Don't delete this
-	override function close():Void
-	{
-		super.close();
-
-		if(finishCallback != null)
-		{
-			finishCallback();
-			finishCallback = null;
-		}
-	}
+		
+	});
 }
